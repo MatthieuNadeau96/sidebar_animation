489a9c9514a80811e0cb3ed84f03af9202163700
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-05-06 14:54:02 -0400

    created onIconPressed function & put parent AnimatedPositioned inside a StreamBuilder to handle if the sidebar is opened

--- lib/sidebar/sidebar.dart
+++ lib/sidebar/sidebar.dart
@@ -13,9 +13,7 @@ class _SidebarState extends State<Sidebar> with SingleTickerProviderStateMixin {
   StreamController<bool> isSideBarOpenedStreamController;
   Stream<bool> isSideBarOpenedStream;
   StreamSink<bool> isSideBarOpenedSink;
-
-  final bool isSidebarOpened = false;
-  final _animationDuration = Duration(milliseconds: 500);
+  final _animationDuration = Duration(milliseconds: 300);
 
   @override
   void initState() {
@@ -35,39 +33,64 @@ class _SidebarState extends State<Sidebar> with SingleTickerProviderStateMixin {
     isSideBarOpenedSink.close();
   }
 
+  void onIconPressed() {
+    final animationStatus = _animationController.status;
+    final isAnimationCompleted = animationStatus == AnimationStatus.completed;
+
+    if (isAnimationCompleted) {
+      isSideBarOpenedSink.add(false);
+      _animationController.reverse();
+    } else {
+      isSideBarOpenedSink.add(true);
+      _animationController.forward();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    return AnimatedPositioned(
-      duration: _animationDuration,
-      top: 0,
-      bottom: 0,
-      left: isSidebarOpened ? 0 : 0,
-      right: isSidebarOpened ? 0 : screenWidth - 50,
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              color: Theme.of(context).primaryColor,
-            ),
-          ),
-          Align(
-            alignment: Alignment(0, -0.9),
-            child: Container(
-              height: 110,
-              width: 35,
-              color: Theme.of(context).primaryColor,
-              alignment: Alignment.centerLeft,
-              child: AnimatedIcon(
-                progress: _animationController.view,
-                icon: AnimatedIcons.menu_close,
-                color: Theme.of(context).canvasColor,
-                size: 25,
+
+    return StreamBuilder<bool>(
+      initialData: false,
+      stream: isSideBarOpenedStream,
+      builder: (context, isSideBarOpenedAsync) {
+        return AnimatedPositioned(
+          duration: _animationDuration,
+          top: 0,
+          bottom: 0,
+          left: isSideBarOpenedAsync.data ? 0 : 0,
+          right: isSideBarOpenedAsync.data ? 0 : screenWidth - 50,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).primaryColor,
+                ),
               ),
-            ),
-          )
-        ],
-      ),
+              Align(
+                alignment: Alignment(0, -0.9),
+                child: GestureDetector(
+                  onTap: () {
+                    onIconPressed();
+                  },
+                  child: Container(
+                    height: 110,
+                    width: 35,
+                    color: Theme.of(context).primaryColor,
+                    alignment: Alignment.centerLeft,
+                    child: AnimatedIcon(
+                      progress: _animationController.view,
+                      icon: AnimatedIcons.menu_close,
+                      color: Theme.of(context).canvasColor,
+                      size: 25,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
